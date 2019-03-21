02522fac0a6d21dddd96172691104700fb2c91e6
Sernandezglez <shernander14@ucol.mx>
2019-03-20 19:21:24 -0600

    Solucion problema 002

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PruebasUnidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ln;
+
+            try
+            {
+                StreamReader sr =
+                new StreamReader("C:/Users/ASKII.DESKTOP-D39ELPS/Documents/gitStuff/automatizar-pruebas-unitarias-2019-Shernandezglez/CasosPrueba.txt");
+
+                ln = sr.ReadLine();
+
+                
+                
+
+                while (ln != null)
+                {
+                    Console.WriteLine(ln);
+                    ln = sr.ReadLine();
+                    
+                }
+                Console.WriteLine(" ");
+                
+                
+                Console.ReadLine();
+            } catch (Exception e)
+
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            Console.WriteLine("Executing finally block.");
+
+
+            
+
+
+        }
+    }
+}
