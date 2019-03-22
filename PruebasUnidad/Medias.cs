8c9ff8a62b8f3df0ead739258c5230823af21f9f
Sernandezglez <shernander14@ucol.mx>
2019-03-21 22:26:32 -0600

    problema 3

---
+++ PruebasUnidad/Medias.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasUnidad
+{
+    class Medias
+    {
+       
+        /**
+        * Calcula y regresa la media artimética prueba
+        */
+        public static double mediaAritmetica(params int[] vals)
+        {
+
+            
+
+        }
+
+        /**
+         * Calcula y regresa la raíz enésima = x^(1/n)
+         */
+        private static double raizEnesima(double x, int n) { return 22; }
+
+        /**
+         *  Usa raizEnesima para calcular y regresar la media geométrica
+         */
+
+            
+        public double mediaGeometrica(params int[] vals) { return 22; }
+
+        /**
+         * Este método no está implementado.
+         */
+        public static double mediaArmonica(params int[] vals) { return 22; }
+    }
+}
--- PruebasUnidad/Program.cs
+++ PruebasUnidad/Program.cs
@@ -13,6 +13,10 @@ namespace PruebasUnidad
         {
             string ln;
             string[] parts = null;
+            int[] en = null;
+            double s = 0;
+            string[] ins = null;
+            
             try
             {
                 StreamReader sr =
@@ -27,15 +31,39 @@ namespace PruebasUnidad
                     Console.WriteLine(ln);
                     parts = ln.Split(':');
                     Console.WriteLine(" ");
-                    for (int i = 0; i <= parts.Length - 1; i++)
+                    for (int i = 0; i <= parts.Length-1; i++)
                     {
                         Console.WriteLine(parts[i]);
+
                     }
                     Console.WriteLine(" ");
+
+                    ins = new string[2];
+
+
+
+                    ins[0] = parts[2];
+                    ins[1] = parts[3];
+                    string c = ins[0];
+                    string c2 = ins[1];
+                    
+                    s = double.Parse(c2);
+                    ins = new string[c.Length];
+                    ins = c.Split(' ');
+                    
+                    
+                    en = new int[ins.Length];
+                    
+                    for(int i = 0; i <= ins.Length-1; i++)
+                    {
+                        en[i] = int.Parse(ins[i]);
+                        
+                    }
+
                     ln = sr.ReadLine();
                     
                 }
-
+                
                 sr.Close();
                 Console.ReadLine();
             } catch (Exception e)
@@ -47,7 +75,7 @@ namespace PruebasUnidad
 
 
             
-
+            
 
         }
     }
